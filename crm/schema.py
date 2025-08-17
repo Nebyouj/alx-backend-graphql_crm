c8cadeb61441e8903e94e962cbcc50abaae33da2
Nebyou Damtew <nebyoudamtew18@gmail.com>
2025-08-17 09:44:54 +0300

    Understanding GraphQL

--- crm/schema.py
+++ crm/schema.py
@@ -123,3 +123,9 @@ class Mutation(graphene.ObjectType):
     bulk_create_customers = BulkCreateCustomers.Field()
     create_product = CreateProduct.Field()
     create_order = CreateOrder.Field()
+
+
+# class Query(graphene.ObjectType):
+#     hello = graphene.String(default_value = "Hello, GraphQL!")
+
+# schema = graphene.Schema(query=Query)
